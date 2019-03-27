92c5b2e7bf3784fa5c4b25ea3757fccd54a6e1f2
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2019-03-27 16:00:26 -0500

    pruebas

---
+++ ejercicios/20/pruebas.py
@@ -0,0 +1,30 @@
+import unittest
+import complejo
+import math
+
+class TestComplejo(unittest.TestCase):
+    def test_conjugado(self):
+        c = complejo.Complejo(2.0,5.0)
+        c.conjugado()
+        self.assertEqual(c.imaginario, -5.0)
+    def test_norma(self):
+        c = complejo.Complejo(0,1.0)
+        c.calcula_norma()
+        self.assertEqual(c.norma, 1.0)
+        c = complejo.Complejo(1.0,0.0)
+        c.calcula_norma()
+        self.assertEqual(c.norma, 1.0)
+    def test_pow(self):
+        c = complejo.Complejo(0, 1.0)
+        d = c.pow(2)
+        self.assertAlmostEqual(d.real,-1.0)
+        self.assertAlmostEqual(d.imaginario,0.0)
+
+        c = complejo.Complejo(0, 1.0)
+        d = c.pow(4)
+        self.assertAlmostEqual(d.real,1.0)
+        self.assertAlmostEqual(d.imaginario,0.0)
+
+if __name__ == '__main__':
+    unittest.main()
+    
